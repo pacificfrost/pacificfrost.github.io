77ddd8732d72d9b3be925d0a40ef37a8210ab4b0
pacificfrost <alisdair.frost@gmail.com>
2020-08-23 22:57:50 -0600

    Init repository

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import './registerServiceWorker';
+
+import App from './App.vue';
+
+import router from './router';
+import store from './store';
+
+import Vue from 'vue';
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
 new Vue({
+  render: h => h(App),
   router,
   store,
-  render: h => h(App)
-}).$mount("#app");
+  vuetify,
+}).$mount('#app');
