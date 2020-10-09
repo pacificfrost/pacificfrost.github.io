d2052de9513d6abeb6ed0b8d63df91e3aaca48b1
pacificfrost <alisdair.frost@gmail.com>
2020-10-08 22:38:10 -0600

    Add update.mixin

---
+++ src/mixins/update.mixin.js
@@ -0,0 +1,37 @@
+export default {
+  data: () => ({
+    refreshing: false,
+    registration: null,
+    updateExists: false,
+  }),
+  created() {
+    document.addEventListener('sw-updated', this.updateAvailable, {
+      once: true,
+    });
+
+    navigator.serviceWorker.addEventListener('controllerchange', () => {
+      if (this.refreshing) {
+        return;
+      }
+
+      this.refreshing = true;
+
+      window.location.reload();
+    });
+  },
+  methods: {
+    updateAvailable(event) {
+      this.registration = event.detail;
+      this.updateExists = true;
+    },
+    refreshApp() {
+      this.updateExists = false;
+
+      if (!this.registration || !this.registration.waiting) {
+        return;
+      }
+
+      this.registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+    },
+  },
+};
